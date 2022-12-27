99da3632e1291391f7b6891d80f82f5ac5585b01
Lenzuolo <lenzuolo@wp.pl>
2022-12-27 21:25:25 +0100

    Initial layout, file structure

---
+++ app/src/modules/StartingPage/StartingPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Layout } from "antd";
+
+const { Content } = Layout;
+
+const StartingPage: React.FC = () => {
+    return ( 
+        <>
+            <Content>
+                Test
+            </Content>
+        </>
+    )
+}
+
+export default StartingPage;
