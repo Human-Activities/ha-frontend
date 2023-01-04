cb3ee4496ade4ae438d2ce8a3759f46aa849d404
Lenzuolo <lenzuolo@wp.pl>
2023-01-04 23:05:58 +0100

    LoginPage layout, small fixes

--- app/src/modules/LoginPage/LoginPage.tsx
+++ app/src/modules/LoginPage/LoginPage.tsx
@@ -1,7 +1,35 @@
+import { Card, Tabs } from "antd";
 import React from "react";
+import LoginForm from "./LoginForm";
+import './LoginPage.scss';
 
 const LoginPage: React.FC = () => {
-    return (<div>test</div>)
+
+    const onFormFinish = (formData: any) => {
+        console.log(formData)
+    }
+    const tabsDef = [
+        {
+            label: 'Register',
+            key: 'register',
+            children: <LoginForm register onFinish={onFormFinish}/> 
+        }, 
+        {
+            label: 'Login',
+            key: 'login',
+            children: <LoginForm onFinish={onFormFinish}/>
+        }
+    ]
+    return (
+        <div className="ha-v-flexbox align-center" style={{margin: 'auto'}}>
+            <div className="ha-v-flexbox align-center form-content">
+                <Tabs
+                    type="card"
+                    items={tabsDef}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default LoginPage;
