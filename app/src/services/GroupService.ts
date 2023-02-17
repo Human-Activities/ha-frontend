a1bdb82f3bc803a1e90cfc83f91ffd933958b731
Lenzuolo <lenzuolo@wp.pl>
2023-02-17 14:56:53 +0100

    Fix services

--- app/src/services/GroupService.ts
+++ app/src/services/GroupService.ts
@@ -6,7 +6,7 @@ export class GroupService {
     
     static async create(group: Group) {
         try {
-            const { data } = await axios.post(`${ApiURL}group/create`, group);
+            const { data } = await axios.post(`${ApiURL}groups/create`, group);
             return data
         } catch (error) {
             return null;
@@ -14,10 +14,21 @@ export class GroupService {
     }
     static async getGroups() {
         try {
-            const { data } = await axios.get(`${ApiURL}group/get`);
+            const { data } = await axios.get(`${ApiURL}groups/get`);
             return data as Group[]
         } catch (error) {
             return [];
         }
+    }
+    static async joinGroup(userGuid: string,url?: string) {
+        try {
+            const {data} = await axios.post(`http://127.0.0.1:8000/pai/${url}`, {user_uuid: userGuid});
+            return {data};
+        } catch(error) {
+            return null;
+        }
+    }
+    static async getGroupLink(groupGuid?: string) {
+        
     }
 }
